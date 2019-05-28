05cc0f3a28c257b8f2e04da2bd226c7a7a2ded75
KimVuu <rhwktjstod@gmail.com>
2019-05-28 15:50:18 +0900

    css, js 최적화

--- static/js/renderer.js
+++ static/js/renderer.js
@@ -1,39 +1,44 @@
 'use strict'
 
 const ioHook = require('iohook')
-
 const { ipcRenderer } = require('electron')
 
-let counter = document.querySelector(".counter")
 let clear
+let ProxyState = { counter: 0 }
 
 const content = document.querySelector("#content")
 
 const Render = ( DOM, element ) => { DOM.innerHTML = element }
 
-const _set = ( n ) => {
-  Render(content, `<div class="counter">${n}</div>`)
-  ipcRenderer.send('order', true)
-}
+const App = ( counter, point ) => `
+  <div class="counter ${CounterColor(counter)}">${counter}</div>
+  ${ point ? `<div class="point">${point}</div>` : `` }
+`
 
-const _clear_timeout = () => {
-  clearTimeout(clear)
-  clear = setTimeout(() => {
-    CounterClear()
-  }, 3000)
+const AppBuild = ( counter, callback ) => {
+  Render(content, App(counter))
+  ipcRenderer.send('order', true)
+  if ( callback ) { callback() }
 }
 
 const CounterUp = () => {
-  ProxyState.counter += 1
-  _set(ProxyState.counter)
-  _clear_timeout()
-}
-const CounterClear = () => {
-  ProxyState.counter = 0
-  _set(ProxyState.counter)
+  let counter = ProxyState.counter += 1
+
+  AppBuild(counter, () => {
+    clearTimeout(clear)
+    clear = setTimeout(() => {
+      CounterClear()
+    }, 3000)
+  })
 }
 
-let ProxyState = { counter: 0 }
+const CounterClear = () => {
+  let counter = ProxyState.counter = 0
+
+  AppBuild(counter)
+}
+
+const CounterColor = ( counter ) => { }
 
 ioHook.on('keypress', event => {
   CounterUp()
